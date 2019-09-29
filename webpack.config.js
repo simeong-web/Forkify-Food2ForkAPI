f77e80b900a96cb123b868b1f1065e9c9c1d630f
simeong-web <52510040+simeong-web@users.noreply.github.com>
2019-09-29 16:27:01 +0100

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -28,16 +28,3 @@ module.exports = {
         ]
     }
 };
-<configuration>
-  <system.webServer>
-    <handlers>
-      <add name="iisnode" path="index.js" verb="*" modules="iisnode" />
-    </handlers>
-    <defaultDocument enabled="true">
-      <files>
-        <add value="index.js" />
-      </files>
-    </defaultDocument>
-    
-  </system.webServer>
-</configuration>
