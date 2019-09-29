1378054397a6c69bf115235878de31908e4682f9
simeong-web <52510040+simeong-web@users.noreply.github.com>
2019-09-29 16:21:23 +0100

    Second Commit
    Adding configuration to solve deployment issues

--- webpack.config.js
+++ webpack.config.js
@@ -28,3 +28,16 @@ module.exports = {
         ]
     }
 };
+<configuration>
+  <system.webServer>
+    <handlers>
+      <add name="iisnode" path="index.js" verb="*" modules="iisnode" />
+    </handlers>
+    <defaultDocument enabled="true">
+      <files>
+        <add value="index.js" />
+      </files>
+    </defaultDocument>
+    
+  </system.webServer>
+</configuration>
