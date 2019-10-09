786a070e3b58f6bac2cd655c1ac8119da829fc9e
simeong-web <52510040+simeong-web@users.noreply.github.com>
2019-10-09 16:32:08 +0100

    Update PORT

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
 import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3000;
 
 /** Global state of the app
  * - Search object
