36b67c3fb995db97944a1d8738bd04cc796dff7a
simeong-web <52510040+simeong-web@users.noreply.github.com>
2019-10-12 08:54:38 +0100

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,2 @@
-export const key = {
-  proxy: 'https://cors-anywhere.herokuapp.com/',
-  key: '25fe3da037b38bfbd2a7ecefe2dbcf22'
-}
+export const key = '25fe3da037b38bfbd2a7ecefe2dbcf22'
+export const proxy = 'https://cors-anywhere.herokuapp.com/'
