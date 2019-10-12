8c4148cd01cc099c73f80e000eda52695ba87c96
simeong-web <52510040+simeong-web@users.noreply.github.com>
2019-10-12 08:59:51 +0100

    Update Recipe.js

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
-import { key, proxy } from '../config';
+import { keys } from '../config';
 
 export default class Recipe {
-    constructor(id) {
-        this.id = id;
-    }
+  constructor(id) {
+      this.id = id;
+  }
 
-    async getRecipe() {
-        try {
-            const res = await axios(`${proxy}http://food2fork.com/api/get?key=${key}&rId=${this.id}`);
-            this.title = res.data.recipe.title;
-            this.author = res.data.recipe.publisher;
-            this.img = res.data.recipe.image_url;
-            this.url = res.data.recipe.source_url;
-            this.ingredients = res.data.recipe.ingredients;
+  async getRecipe() {
+      try {
+      const res = await axios(`https://www.food2fork.com/api/get?key=${keys.key5}&rId=${this.id}`)
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
         } catch (error) {
             console.log(error);
             alert('Something went wrong :(');
